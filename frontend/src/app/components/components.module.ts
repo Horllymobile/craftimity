b9bb4829d29ddda88c38b79fc4a49d5c81196a07
Abraham Olamide James <horlamidex1@gmail.com>
2023-09-02 16:28:39 +0100

    Update login page and component

--- frontend/src/app/components/components.module.ts
+++ frontend/src/app/components/components.module.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { SharedModule } from "../core/shared/shared.module";
 import { MaterialModule } from "../core/shared/material.module";
 import { LoginComponent } from "./login/login.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [NavbarComponent, LoginComponent],
-  imports: [CommonModule, SharedModule, MaterialModule],
+  imports: [CommonModule, SharedModule, MaterialModule, RouterModule],
   exports: [NavbarComponent],
 })
 export class ComponentsModule {}
