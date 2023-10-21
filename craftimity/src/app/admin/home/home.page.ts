ac1db57d560f4a0ea242447961146367182a2d10
Abraham Olamide James <horlamidex1@gmail.com>
2023-10-21 17:29:58 +0100

    Added category setup and menu

--- craftimity/src/app/admin/home/home.page.ts
+++ craftimity/src/app/admin/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
 import { ICategory } from 'src/app/core/models/category';
 import { IUser } from 'src/app/core/models/user';
+import { CategoryService } from 'src/app/core/services/category/category.service';
 import { UsersService } from 'src/app/core/services/users/users.service';
 
 @Component({
@@ -10,68 +12,20 @@ import { UsersService } from 'src/app/core/services/users/users.service';
 })
 export class HomePage implements OnInit {
   userData!: IUser;
-  categories!: ICategory[];
-  constructor(private usersService: UsersService) {}
+  categories$!: Observable<ICategory[]>;
+  page = 1;
+  size = 20;
+  constructor(
+    private usersService: UsersService,
+    private categothryService: CategoryService
+  ) {}
 
   ngOnInit() {
     this.userData = this.usersService.userProfile;
 
-    this.categories = [
-      {
-        name: 'Plumber',
-        icon: 'assets/svg/plumber.svg',
-        active: true,
-      },
-      {
-        name: 'Tilers',
-        icon: 'assets/svg/wall.svg',
-        active: true,
-      },
-      {
-        name: 'Bricklayers',
-        icon: 'assets/svg/hard-hat.svg',
-        active: true,
-      },
-      {
-        name: 'Mechanics',
-        icon: 'assets/svg/mechanics.svg',
-        active: true,
-      },
-      {
-        name: 'Painters',
-        icon: 'assets/svg/painter.svg',
-        active: true,
-      },
-      {
-        name: 'Carpenters',
-        icon: 'assets/svg/carpenter.svg',
-        active: true,
-      },
-      {
-        name: 'Health workers',
-        icon: 'assets/svg/protection.svg',
-        active: true,
-      },
-      {
-        name: 'Electricians',
-        icon: 'assets/svg/electrician.svg',
-        active: true,
-      },
-      {
-        name: 'Rewires',
-        icon: 'assets/svg/wire.svg',
-        active: true,
-      },
-      {
-        name: 'AC technicians',
-        icon: 'assets/svg/appliance-repair.svg',
-        active: true,
-      },
-      {
-        name: 'Computer technicians',
-        icon: 'assets/svg/technician.svg',
-        active: true,
-      },
-    ];
+    this.categories$ = this.categothryService.getCategories({
+      page: this.page,
+      size: this.size,
+    });
   }
 }
--- craftimity/src/app/core/models/category.ts
+++ craftimity/src/app/core/models/category.ts
@@ -2,4 +2,7 @@ export interface ICategory {
   name: string;
   icon: string;
   active: boolean;
+  created_at?: string;
+  created_by?: string;
+  updated_at?: string;
 }
