43d1545e0ee0652f25b63932ff402bc84ec80c3b
Abraham Olamide James <horlamidex1@gmail.com>
2024-01-03 09:29:15 +0100

    Fix contact responsive issue

--- website/src/app/contact/contact.component.ts
+++ website/src/app/contact/contact.component.ts
@@ -1,65 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { EmailService } from '../core/services/email.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { finalize } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
-  providers: [HttpClient],
 })
-export class ContactComponent {
-  isLoading = false;
-  form!: FormGroup;
-
-  constructor(private emailService: EmailService) {
-    this.form = new FormGroup({
-      first_name: new FormControl(null, [Validators.required]),
-      last_name: new FormControl(null, [Validators.required]),
-      email: new FormControl(null, [Validators.required]),
-      phone: new FormControl(null, [Validators.required]),
-      message: new FormControl(null, [Validators.required]),
-    });
-  }
-
-  async submitContact(payload: {
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    message: string;
-  }) {
-    this.isLoading = true;
-
-    const sent = await this.emailService
-      .addContact({
-        FirstName: payload.first_name,
-        LastName: payload.last_name,
-        Email: payload.email,
-        Status: 'Transactional',
-        CustomFields: {
-          message: payload.message,
-          phone: payload.phone,
-        },
-      })
-      .pipe(finalize(() => (this.isLoading = false)))
-      .subscribe({
-        next: (res) => {
-          console.log(res);
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
-  }
-}
+export class ContactComponent {}
